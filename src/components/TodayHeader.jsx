feae81d5ced1099305eb7d419968b13bd05407fd
immanuellking <emmaebubeegbosi@gmail.com>
2023-06-21 07:25:17 +0100

    added the first countdown functionality

--- src/components/TodayHeader.jsx
+++ src/components/TodayHeader.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {BsArrowRight, BsArrowLeft} from "react-icons/bs";
 import Tag from './Tag';
 
-const TodayHeader = ({handleScroll}) => {
+const TodayHeader = ({handleScroll, targetDate}) => {
+
+    const [timeLeft, setTimeLeft] = useState(getTimeLeft(targetDate));
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+          setTimeLeft(getTimeLeft(targetDate));
+        }, 1000);
+    
+        return () => {
+          clearInterval(timer);
+        };
+      }, [targetDate]);
+
+    function getTimeLeft (targetDate) {
+        const totalSeconds = Math.floor((new Date() - targetDate) / 1000);
+        console.log(`TotalSecond: ${totalSeconds}`);
+
+        const days = Math.abs(Math.floor(totalSeconds / 86400));
+        console.log(`Days: ${days}`);
+
+        const hours = Math.abs(Math.floor((totalSeconds % 86400) / 3600));
+        console.log(`Hours: ${hours}`);
+
+        const minutes = Math.abs(Math.floor((totalSeconds % 3600) / 60));
+        console.log(`Minutes: ${minutes}`);
+
+        const seconds = Math.abs(Math.floor(totalSeconds % 60));
+        console.log(`Seconds: ${seconds}`);
+
+        return {
+            days,
+            hours,
+            minutes,
+            seconds
+        }
+    }
+
+    const {days, hours, minutes, seconds} = timeLeft;
+
   return (
     <div className="flex flex-col space-y-6 sm:space-y-9 ">
         <Tag title={"Today's"} />
@@ -12,19 +51,19 @@ const TodayHeader = ({handleScroll}) => {
                 <div className="flex space-x-1 lg:space-x-8 items-center justify-center">
                     <div className="time">
                         <span className="time-tag">Days</span>
-                        <h2 className="time-text">03</h2>
+                        <h2 className="time-text">{days}</h2>
                     </div> <span className="time-text text-brightRed">:</span>
                     <div className="time">
                         <span className="time-tag">Hours</span>
-                        <h2 className="time-text">23</h2>
+                        <h2 className="time-text">{hours}</h2>
                     </div> <span className="time-text text-brightRed">:</span>
                     <div className="time">
                         <span className="time-tag">Minutes</span>
-                        <h2 className="time-text">19</h2>
+                        <h2 className="time-text">{minutes}</h2>
                     </div> <span className="time-text text-brightRed">:</span>
                     <div className="time">
                         <span className="time-tag">Seconds</span>
-                        <h2 className="time-text">56</h2>
+                        <h2 className="time-text">{seconds}</h2>
                     </div>
                 </div>
             </div>
--- src/components/Todays.jsx
+++ src/components/Todays.jsx
@@ -15,9 +15,11 @@ const Todays = () => {
         }
     }
 
+    const targetDate = new Date('2023-07-15T23:59:59');
+
   return (
     <div className=" px-3 my-12 lg:px-12 lg:my-20 w-full">
-        <TodayHeader handleScroll={handleScroll}/>
+        <TodayHeader handleScroll={handleScroll} targetDate={targetDate}/>
         <Products slideRef={slideRef} />
         <Button title={"View All Products"}/>
     </div>
